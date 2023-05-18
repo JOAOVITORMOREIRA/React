3d243a39d6b23a1f93c159e6d7bd2a7fe9ce9138
João Vitor Moreira <joaoitagv@gmail.com>
2023-05-18 15:10:56 -0300

    Componete Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/estaticos/navbar/Navbar'
 import Footer from './components/estaticos/footer/Footer'
 import Home from './paginas/home/Home'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './paginas/login/Login'
 
 function App() {
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -7,24 +7,24 @@ import Carrossel from "../../components/carrossel/Carrossel";
 function Home() {
     return (
         <>
-            {/* <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor:"#E0AAFF" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
                 <Grid alignItems="center" item xs={6}>
-                    <Box padding={20}>
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color:"white", fontWeight:"bold" }}>Seja Bem Vindo</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color:"white", fontWeight:"bold" }}>Expresse aqui sua raiva</Typography>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor:"white", backgroundColor:"#2A1268", color:"white" }}>Ver Postagens</Button>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
                     </Box>
                 </Grid>
-                <Grid item xs={6}>
-                    <img src="https://th.bing.com/th/id/R.4d2bd7287c3f4e0ff63f6c7ac98c0c34?rik=wLAEfWze7n4tjA&pid=ImgRaw&r=0" alt="" width="600px" height="600px" />
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor:"white"}}>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
                 </Grid>
-            </Grid> */}
+            </Grid>
 
             <Grid container style={{ marginTop: "8px" }}>
                 <Grid item xs={12}>
