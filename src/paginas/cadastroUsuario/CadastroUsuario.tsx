bb63305711d06b24d3bd2138ee158e19a69dc1f8
João Vitor Moreira <joaoitagv@gmail.com>
2023-05-18 19:38:41 -0300

    Componente Login 2

--- src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -7,11 +7,11 @@ import './CadastroUsuario.css';
 function CadastroUsuario() {
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
-            <Grid item xs={6} className='imagem2'></Grid>
+            <Grid item xs={6} className='imagemCadastro'></Grid>
             <Grid item xs={6} alignItems='center'>
                 <Box paddingX={10}>
                     <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos2'>Cadastrar</Typography>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textoCadastro'>Cadastrar</Typography>
                         <TextField id='nome' label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
                         <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal'fullWidth />
                         <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
