1d1b7da4bbb629f09d8f77777095b627a10c1100
João Vitor Moreira <joaoitagv@gmail.com>
2023-05-24 10:33:29 -0300

    Criação elementos Post Put Postagem

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Grid, Typography } from '@material-ui/core'
 import { Box } from '@mui/material'
 import './Home.css'
 import Carrossel from "../../components/carrossel/Carrossel";
 import TabPostagem from "../../components/postagens/tabPostagem/TabPostagem";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+import { useNavigate } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 
 function Home() {
+
+    
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    
+    useEffect(() => {
+      if (token == "") {
+          alert("Você precisa estar logado")
+          navigate("/login")
+  
+      }
+  }, [token])
+
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#404040" }}>
@@ -16,6 +32,7 @@ function Home() {
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
+                            <ModalPostagem />
                         </Box>
                         <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#b70000", color: "white"}}>Ver Postagens</Button>
                     </Box>
